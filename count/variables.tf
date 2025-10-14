925cfa3414903fa77e40b98f16e3f7b9e90a84bb
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-14 18:20:13 +0530

    Create variables.tf

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+  default = "Z06390712DSNMTA6S2AHD"
+}
+
+variable "domain_name" {
+  default = "pavandevops.fun"
+}
