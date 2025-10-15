fcade30caa102212430d505a7fc9b988199bf1d9
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-15 16:09:59 +0530

    Update r53.tf

--- for-loop/r53.tf
+++ for-loop/r53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "roboshop" {
-  for_each = aws_instance.terrafrom
+  for_each = aws_instance.terraform
   zone_id = var.zone_id
   name = "${each.key}.${var.domain_name}"
   type = "A"
