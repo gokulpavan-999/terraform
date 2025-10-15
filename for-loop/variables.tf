7dfb49ff31fb44260d268bdff4c3729defddf8b6
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-15 15:39:59 +0530

    Update variables.tf

--- for-loop/variables.tf
+++ for-loop/variables.tf
@@ -1,1 +1,28 @@
+variable "instances" {
+  default = [ "mongodb", "redis" ] # list type 
 
+ # for map type:-
+ # default = {
+ #   mongodb = "t3.micro"
+ #   redis = "t3.micro"
+ #    mysql = "t3.micro"
+ # }
+
+ # default = {
+ #   mongodb = {
+ #     instance_type = "t3.micro"
+ #     ami = "ami-id"
+ #   }
+ #   redis = "t3.micro"
+ #   mysql = "t3.small"
+ # }
+
+}
+
+variable "zone_id" {
+  default = "Z06390712DSNMTA6S2AHD"
+}
+
+variable "domain_name" {
+  default = "pavandevops.fun"
+}
