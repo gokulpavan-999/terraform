d0421f387a32e0e8369546f462fbd2ab24b96042
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-17 14:35:09 +0530

    Update ec2.tf

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -18,9 +18,25 @@ provisioner "local-exec"{
 }
 
 connection {
-  type    = "ssh"
-  user    = "ec2-user"
-  password= 
+  type     = "ssh"
+  user     = "ec2-user"
+  password = "DevOps321"
+  host     = self.public_ip
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo dnf install nginx -y"
+    "sudo systemctl start nginx"
+  ]
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo systemctl stop nginx"
+    "echo 'successfully stopped nginx server' "
+  ]
+  when = destroy
 }
 
 }
